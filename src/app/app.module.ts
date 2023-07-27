97bde85be2e34522b88eb96f5f6910eb19b33e70
gaurav sonawane <124447286+gauravsonawane12@users.noreply.github.com>
2023-07-27 13:01:45 +0530

    work done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Headercomponet } from './header/header.component';
+import { Navbarcomponenet } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Headercomponet,
+    Navbarcomponenet,
   ],
   imports: [
     BrowserModule
